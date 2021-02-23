aacdf03b50bd15b874b227914b00bcb5a93960d2
noriyoshimaeda <noriyoshimaeda@gmail.com>
2021-02-24 02:42:40 +0900

    3rd commit

--- Lab07/config.py
+++ Lab07/config.py
@@ -1,6 +1,6 @@
 ecs_test_drive = {
     'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
-    'ecs_access_key_id' : '132542226243614727@ecstestdrive.emc.com',
-    'ecs_secret_key' : '3UNYjtYNYB3bMuyGbkfvmu1m3sNR1VA/Jj1+CEw1',
+    'ecs_access_key_id' : '1234-your-unique-number-5678@ecstestdrive.emc.com',
+    'ecs_secret_key' : 'your-long-secret-key-from-ECS-testdrive-portal',
     'ecs_bucket_name' : 'photo-album'
 }
--- Lab07/models.py
+++ Lab07/models.py
@@ -24,8 +24,7 @@ from config import ecs_test_drive
 
 # Otherwise, assume running locally with local MongoDB instance
 #else:
-#client = MongoClient("mongodb://USERNAME:PASSWORD@cluster0-shard-00-00.mzmpq.mongodb.net:27017,cluster0-shard-00-01.mzmpq.mongodb.net:27017,cluster0-shard-00-02.mzmpq.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qm2w8-shard-0&authSource=admin&retryWrites=true&w=majority")
-client = MongoClient("mongodb+srv://maedan:dellPIPERtraining@cluster0.ibrn3.mongodb.net,cluster0.ibrn3.mongodb.net,cluster0.ibrn3.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb://USERNAME:PASSWORD@cluster0-shard-00-00.mzmpq.mongodb.net:27017,cluster0-shard-00-01.mzmpq.mongodb.net:27017,cluster0-shard-00-02.mzmpq.mongodb.net:27017/?ssl=true&replicaSet=atlas-4qm2w8-shard-0&authSource=admin&retryWrites=true&w=majority")
 DB_NAME = "testdb1"  ##### Make sure this create your unique MongoDB database name######
 
 # Get database connection with database name
